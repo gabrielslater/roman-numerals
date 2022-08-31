571f405ea93a9ddd566dae7b965501c8e0c73f4d
gabriel slater <gabrielxslater@gmail.com>
2022-08-31 09:52:42 -0400

    feat: 'x' = 10 green

--- lib/roman_numeral.dart
+++ lib/roman_numeral.dart
@@ -2,6 +2,8 @@ class RomanNumeral {
   static toArabic(String numeral) {
     if (numeral == "V") {
       return 5;
+    } else if (numeral == "X") {
+      return 10;
     } else {
       return 1;
     }

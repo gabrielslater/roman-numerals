a4c42bcca14960732aa2dd3f7d2c9e17d76be27d
gabriel slater <gabrielxslater@gmail.com>
2022-08-31 09:58:36 -0400

    style: converted to single-quotes

--- lib/roman_numeral.dart
+++ lib/roman_numeral.dart
@@ -2,9 +2,9 @@ class RomanNumeral {
   // information taken from
   // https://en.wikipedia.org/wiki/Roman_numerals#Standard_form
   static toArabic(String numeral) {
-    if (numeral == "V") {
+    if (numeral == 'V') {
       return 5;
-    } else if (numeral == "X") {
+    } else if (numeral == 'X') {
       return 10;
     } else {
       return 1;

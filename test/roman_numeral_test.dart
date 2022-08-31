0e47f1e8eccb2e5a0da92c03156067b32e422327
gabriel slater <gabrielxslater@gmail.com>
2022-08-31 09:43:41 -0400

    refactor: d.r.y. for test cases

--- test/roman_numeral_test.dart
+++ test/roman_numeral_test.dart
@@ -2,12 +2,14 @@ import 'package:roman_numerals/roman_numeral.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('I = 1', () {
-    var result = RomanNumeral.toArabic("I");
-    expect(result, equals(1));
-  });
-  test('V = 5', () {
-    var result = RomanNumeral.toArabic("V");
-    expect(result, equals(5));
-  });
+  var table = {"I": 1, "V": 5};
+
+  int result;
+
+  for (var key in table.keys) {
+    test('$key = ${table[key]}', () {
+      result = RomanNumeral.toArabic(key);
+      expect(result, equals(table[key]));
+    });
+  }
 }

c05538402481920478d4cc4f823d7ae8e3a4eae8
gabriel slater <gabrielxslater@gmail.com>
2022-08-31 10:02:19 -0400

    refactor: removed else/if and replaced with a map

--- lib/roman_numeral.dart
+++ lib/roman_numeral.dart
@@ -1,13 +1,15 @@
 class RomanNumeral {
+  static var numerals = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+  };
+
   // information taken from
   // https://en.wikipedia.org/wiki/Roman_numerals#Standard_form
   static toArabic(String numeral) {
-    if (numeral == 'V') {
-      return 5;
-    } else if (numeral == 'X') {
-      return 10;
-    } else {
-      return 1;
+    if (numerals.keys.contains(numeral)) {
+      return numerals[numeral];
     }
   }
 }
